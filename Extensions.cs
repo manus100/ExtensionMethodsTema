a27dd8ca1759982199485073e3d82472f85c58ba
Manuela STOIAN <manuela_s100@yahoo.com>
2022-01-20 14:22:36 +0200

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsTema
+{
+    public static class Extensions
+    {
+
+        //Write an extension method to check if a string starts with a capital letter or not.
+        public static bool CheckIfCapital(this string strToCheck)
+        {
+            char chr = strToCheck[0];
+            return Char.IsUpper(chr);
+
+            //if (strToCheck[0] == strToCheck.ToUpper()[0])
+            //    return true;
+            //else
+            //    return false;
+
+        }
+
+        //Write an extension method that appends an ‘s’ to a string.
+        public static string PluralizeString(this string str)
+        {
+            return string.Concat(str, 's');
+        }
+
+        //Write an extension method that will capitalize a word.
+        public static string CapitalizeWord(this string str)
+        {
+            // return string.Concat(char.ToUpper(str[0]), str.Substring(1));
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+
+
+        //Write an extension method what will count the number of characters in a string.
+        public static int CharacterCount(this string str)
+        {
+            return str.Length;
+        }
+
+        //Write an extension method that will apply a VAT of 24% on an input. This input can be a decimal value, or an integer on
+        public static decimal ApplyVAT(this decimal input) 
+        {
+
+            return input + input * 24 / 100;
+        }
+
+        //public static int ApplyVAT(this int input)
+        //{
+
+        //    return input + input * 24 / 100;
+        //}
+
+
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsTema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "ana are mere";
+            string str = "method";
+            decimal value;
+
+            var newList = Swap<string>(new List<string> { "abc", "def", "ghi", "jklm" }, 1, 3);
+            var newListInt = Swap<int>(new List<int> { 1, 2, 3, 4, 5 }, 0, 2);
+
+            Console.WriteLine("Write an extension method to check if a string starts with a capital letter or not.");
+            Console.WriteLine(sentence + "--> " + sentence.CheckIfCapital());
+            Console.WriteLine();
+
+
+            Console.WriteLine("Write an extension method that appends an ‘s’ to a string.");
+            Console.WriteLine(str + "--> " + str.PluralizeString()); 
+            Console.WriteLine();
+
+            Console.WriteLine("Write an extension method that will capitalize a word.");
+            str = "mom";
+            Console.WriteLine(str + "--> " + str.CapitalizeWord());
+            Console.WriteLine();
+
+            //Capitalize and pluralize
+            Console.WriteLine("Capitalize and pluralize a string");
+            str = "rabbit";
+            Console.Write(str + "--> ");
+            str = str.CapitalizeWord();
+            Console.WriteLine(str.PluralizeString());
+            Console.WriteLine();
+
+            //Character count
+            Console.WriteLine("Write an extension method what will count the number of characters in a string.");
+            Console.WriteLine(sentence + "-->" + sentence.CharacterCount());
+            Console.WriteLine();
+
+            //Apply VAT
+            Console.WriteLine("Write an extension method that will apply a VAT of 24% on an input. This input can be a decimal value");
+            Console.WriteLine("Enter a decimal value: ");
+            value = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(value + " + 24%VAT " + value.ApplyVAT());
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+        //Create a generic method that receives a list containing any type of data and swaps the elements at two given indexes.
+        static List<T> Swap<T>(List<T> list, int idx1, int idx2)
+        {
+            var temp = list[idx1];
+            list[idx1] = list[idx2];
+            list[idx2] = temp;
+
+            return list;
+        }
+    }
+}
